c6090f0e9c07706c472a7b47b9d6d7d2320b5dd8
2ich <witkore@mail.ru>
2022-01-05 14:12:44 +0300

    janky

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 // 60 frames per second in ms ? : 1000 / 60 == 16.66..
 // 1000 / 100 == 10
-const updaterate = 250
+const updaterate = Math.round(1000 / 60)
 
 players = {}
 
@@ -24,24 +24,48 @@ function rint(min = 0, max) {
     return min + Math.floor(Math.random() * (max + 1 - min));
 }
 
+
+
+
+
+
 function game() {
     //io.emit('game', 'hello')
-    io.to('general').emit('game', players, 'general game emit')
+    io.to('general').emit('game', players)
 }
+
+
+
+
+
 
 io.on('connection', (socket) => {
     console.log('Made socket connection with ID: ' + socket.id)
     socket.join('general')
-    players[[socket.id]] = { 'pos' : { 'x' : rint(0, 500), 'y' : rint(0, 500) }}
+    players[[socket.id]] = { 'pos' : { 'x' : rint(0, 200), 'y' : rint(0, 200) }}
     console.log(players)
     socket.emit('con', players)
     // io.emit('game', 'hello')
     // io.to('general').emit('game', 'general emit')
 
-    setInterval(game, updaterate)
+    socket.on('gack', (pos) => {
+        console.log(pos)
+        players[[socket.id]]['pos']['x'] = pos['x']
+        players[[socket.id]]['pos']['y'] = pos['y']
+        console.log(players)
+    })
+
+    socket.on('disconnecting', () => {
+        // if (players[[socket.id]] !== undefined)
+        // if (players.hasOwnProperty(socket.id))
+        delete players[[socket.id]]
+    })
+
 
 })
 
 server.listen(port, () => {
     console.log(`Socket.IO server running at http://localhost:${port}/`);
 })
+
+setInterval(game, updaterate)
